b9bc4396fe5f5e542b6969911d23d2d7cc09421c
mantonoiu <antonoiumadalin@gmail.com>
2021-03-26 09:29:40 +0000

    visual testing done, all tests passing

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Counter.css"
 
 function Counter() {
+
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState(1);
+
+
   return (
     <div>
       <h3 data-testid="header">My Counter</h3>
+      <h3 data-testid="counter">{counter}</h3>
+      <button data-testid="subtract-btn">-</button>
+      <input data-testid="input" type="number" value={input} className="text-center" />
+      <button data-testid="add-btn">+</button>
     </div>
   );
 }
