d76b2093f7d65bd7282ea4da1afb5af0cda0f8fa
mantonoiu <antonoiumadalin@gmail.com>
2021-03-26 09:38:24 +0000

    ..

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -12,7 +12,13 @@ function Counter() {
       <h3 data-testid="header">My Counter</h3>
       <h3 data-testid="counter">{counter}</h3>
       <button data-testid="subtract-btn">-</button>
-      <input data-testid="input" type="number" value={input} className="text-center" />
+      <input
+        data-testid="input"
+        type="number"
+        value={input}
+        className="text-center"
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button data-testid="add-btn">+</button>
     </div>
   );
